eb3b6c99330b059518f17028e2aafedb6b25a504
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-04 09:54:58 +0000

    change icon images to jpeg, add state to project gallery

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -5,19 +5,24 @@ import "../styles/styles.css";
 import projects from "../projects.json";
 
 export default class ProjectGallery extends Component {
+  state = {
+    projectCounter: 0,
+    projects
+  }
 
   render() {
+
     return (
       <section className="col-12 col-lg-8 mx-auto p-3">
         <h2>Projects</h2>
-        <div>
+        <div id="project-card">
             <div className="row d-flex flex-column">
                 <div className="col-12">
-                    {projects[0].title}
+                    {projects[this.state.projectCounter].title}
                 </div>
                 <div className="d-flex flex-column flex-md-row-reverse col-12 justify-content-etween">
                     <div className="col-12 col-md-6 col-lg-4">
-                        <img src={projects[0].screenshot} alt={projects[0].title} />
+                        <img src={projects[this.state.projectCounter].screenshot} alt={projects[this.state.projectCounter].title} />
                     </div>
                     <div className="d-flex flex-column col-12 col-md-6 col-lg-8">
                         <div className="col-12">
@@ -26,18 +31,22 @@ export default class ProjectGallery extends Component {
                         </div>
                         <div className="d-flex flex-row justify-content-evenly text-center">
                             <div className="col-6">
-                                <a href={projects[0].deployed}>
+                                <a href={projects[this.state.projectCounter].deployed}>
                                     <i class="fas fa-globe"></i>
                                 </a>
                             </div>
                             <div className="col-6">
-                                <a href={projects[0].repo}>
+                                <a href={projects[this.state.projectCounter].repo}>
                                     <i className="fab fa-github"></i>
                                 </a>
                             </div>
                         </div>
                         <div className="col-12 d-flex flex-row justify-content-evenly">
-                            <p> tech </p>
+                            {this.state.projects[this.state.projectCounter].tech.map(technology => {
+                                return <div>
+                                    <img src={technology} alt="#" />
+                                </div>
+                            })}
                         </div>
                     </div>
                 </div>
