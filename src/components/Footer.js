6c77ff8d3f9a4f0b9c3ad1af9227fe3cab0dd451
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-06 23:31:51 +0000

    layout change after tried deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import "../styles/Footer.css";
 export default class Footer extends Component {
   render() {
     return (
-      <div id="footer" className="row mx-0">
-        <div className="row m-4 text-center">
+      <div id="footer" className="row mx-0 col-12">
+        <div className="row m-0 text-center">
           <p className="m-0">© Nick Misselbrook 2023</p>
         </div>
       </div>
